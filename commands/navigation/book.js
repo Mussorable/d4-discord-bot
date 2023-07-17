628db083d747d74cbd8f37c63eff084c5393b5e8
Mussorable <redduck5601@gmail.com>
2023-07-17 06:50:54 +0200

    added select menu and interaction select/button handler

--- commands/navigation/book.js
+++ commands/navigation/book.js
@@ -1,7 +1,7 @@
 const {
   SlashCommandBuilder,
-  ButtonBuilder,
-  ButtonStyle,
+  StringSelectMenuOptionBuilder,
+  StringSelectMenuBuilder,
   ActionRowBuilder,
 } = require("discord.js");
 
@@ -10,15 +10,28 @@ module.exports = {
     .setName("book")
     .setDescription("Main Menu Navigation Page"),
   async execute(interaction) {
-    const classesButton = new ButtonBuilder()
-      .setCustomId("classesButton")
-      .setLabel("Classes")
-      .setStyle(ButtonStyle.Primary);
+    const select = new StringSelectMenuBuilder()
+      .setCustomId("starter")
+      .setPlaceholder("Choose the section you want to explore.")
+      .addOptions(
+        new StringSelectMenuOptionBuilder()
+          .setLabel("Classes")
+          .setDescription("All about in Diablo 4")
+          .setValue("classes"),
+        new StringSelectMenuOptionBuilder()
+          .setLabel("Utilities")
+          .setDescription("3rd person support utilities")
+          .setValue("utils"),
+        new StringSelectMenuOptionBuilder()
+          .setLabel("Builds")
+          .setDescription("Trending builds in Diablo 4")
+          .setValue("builds")
+      );
 
-    const row = new ActionRowBuilder().addComponents(classesButton);
+    const row = new ActionRowBuilder().addComponents(select);
 
     await interaction.reply({
-      content: "-- MENU --",
+      content: `Welcome back, ${interaction.user.username}! Select the section you are interested in.`,
       components: [row],
     });
   },
